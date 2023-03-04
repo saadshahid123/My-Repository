6caeafeaa46c446db3ce4aed125206fe58b68095
ssuetcs217saadshahid <saadshahidkhann@gmail.com>
2023-03-04 17:56:17 +0500

    sub

--- cypress/e2e/new.cy.js
+++ cypress/e2e/new.cy.js
@@ -12,7 +12,7 @@ cy.xpath("//input[@id='pass-popup']").type("Admin!23")
 cy.get("button[id='btn-social-login-authentication'] span").click()
 cy.wait(1000)
 cy.get("#search").type("EIM-2277437")
-cy.get('.actions > .action').click()
+cy.get('.actions > .action').click()//
 cy.wait(500)
 cy.get("img[alt='دريل مطرقة للحفر في الطوب والحديد والخشب ، 13 مم(1/2 بوصة) ، 710 وات، 0-3200 دورة في الدقيقة، 2.1 كجم ']").click()
 cy.wait(500)
